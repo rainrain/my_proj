fc73b3a13e5ece78d2e15203c90cf79053f14878
baiyu <baiyu@yodo1.com>
2015-09-18 16:46:37 +0800

    add games

---
+++ wdgames/maomi/46.js
@@ -0,0 +1,240 @@
+{
+  "log": {
+    "version": "1.2",
+    "creator": {
+      "name": "WebInspector",
+      "version": "537.36"
+    },
+    "pages": [
+      {
+        "startedDateTime": "2015-09-18T07:44:20.326Z",
+        "id": "page_1",
+        "title": "file:///Users/rain/Downloads/maomi/index.htm",
+        "pageTimings": {
+          "onContentLoad": 654.9370000138879,
+          "onLoad": 3229.107999999542
+        }
+      }
+    ],
+    "entries": [
+      {
+        "startedDateTime": "2015-09-18T07:44:20.335Z",
+        "time": 641.7059999948833,
+        "request": {
+          "method": "GET",
+          "url": "http://app.46465.com/js/46.js",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Accept-Encoding",
+              "value": "gzip, deflate, sdch"
+            },
+            {
+              "name": "Host",
+              "value": "app.46465.com"
+            },
+            {
+              "name": "Accept-Language",
+              "value": "zh-CN,zh;q=0.8,en;q=0.6"
+            },
+            {
+              "name": "User-Agent",
+              "value": "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.20 Mobile Safari/537.36"
+            },
+            {
+              "name": "Accept",
+              "value": "*/*"
+            },
+            {
+              "name": "Cache-Control",
+              "value": "max-age=0"
+            },
+            {
+              "name": "Cookie",
+              "value": "yunsuo_session_verify=dbf3ac6f12a4a94529e4b9e43b7321fb"
+            },
+            {
+              "name": "Connection",
+              "value": "keep-alive"
+            }
+          ],
+          "queryString": [],
+          "cookies": [
+            {
+              "name": "yunsuo_session_verify",
+              "value": "dbf3ac6f12a4a94529e4b9e43b7321fb",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            }
+          ],
+          "headersSize": 404,
+          "bodySize": 0
+        },
+        "response": {
+          "status": 200,
+          "statusText": "OK",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Date",
+              "value": "Fri, 18 Sep 2015 07:44:05 GMT"
+            },
+            {
+              "name": "Content-Encoding",
+              "value": "gzip"
+            },
+            {
+              "name": "Server",
+              "value": "Microsoft-IIS/7.5"
+            },
+            {
+              "name": "X-Powered-By",
+              "value": "ASP.NET"
+            },
+            {
+              "name": "Content-Length",
+              "value": "454"
+            },
+            {
+              "name": "Vary",
+              "value": "Accept-Encoding"
+            },
+            {
+              "name": "Content-Type",
+              "value": "text/html"
+            }
+          ],
+          "cookies": [],
+          "content": {
+            "size": 517,
+            "mimeType": "text/html",
+            "compression": 63,
+            "text": "<!DOCTYPE html><html xmlns=\"http://www.w3.org/1999/xhtml\"><head><meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\"/><meta http-equiv=\"pragma\" content=\"no-cache\"/><meta http-equiv=\"cache-control\" content=\"no-store\"/><meta http-equiv=\"Connection\" content=\"Close\"/><script>function YunSuoAutoJump() { self.location = \"/html5/404.htm?404;http://app.46465.com:80/js/46.js&yunsuo_session_verify=dbf3ac6f12a4a94529e4b9e43b7321fb\"; }</script><script>setTimeout(\"YunSuoAutoJump()\", 300);</script></head></html>"
+          },
+          "redirectURL": "",
+          "headersSize": 199,
+          "bodySize": 454,
+          "_transferSize": 653
+        },
+        "cache": {},
+        "timings": {
+          "blocked": 0.670999987050891,
+          "dns": 2.663999999640509,
+          "connect": 110.3910000238106,
+          "send": 0.06999998004098984,
+          "wait": 527.040000015404,
+          "receive": 0.8699999889363426,
+          "ssl": -1
+        },
+        "connection": "483910",
+        "pageref": "page_1"
+      },
+      {
+        "startedDateTime": "2015-09-18T07:44:20.336Z",
+        "time": 3219.2740000027698,
+        "request": {
+          "method": "GET",
+          "url": "http://img.tongji.linezing.com/3455448/tongji.gif",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Accept-Encoding",
+              "value": "gzip, deflate, sdch"
+            },
+            {
+              "name": "Host",
+              "value": "img.tongji.linezing.com"
+            },
+            {
+              "name": "Accept-Language",
+              "value": "zh-CN,zh;q=0.8,en;q=0.6"
+            },
+            {
+              "name": "User-Agent",
+              "value": "Mozilla/5.0 (Linux; Android 5.1.1; Nexus 6 Build/LYZ28E) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.20 Mobile Safari/537.36"
+            },
+            {
+              "name": "Accept",
+              "value": "image/webp,image/*,*/*;q=0.8"
+            },
+            {
+              "name": "Cache-Control",
+              "value": "max-age=0"
+            },
+            {
+              "name": "Cookie",
+              "value": "lstat_bc=16896125341102131223"
+            },
+            {
+              "name": "Connection",
+              "value": "keep-alive"
+            }
+          ],
+          "queryString": [],
+          "cookies": [
+            {
+              "name": "lstat_bc",
+              "value": "16896125341102131223",
+              "expires": null,
+              "httpOnly": false,
+              "secure": false
+            }
+          ],
+          "headersSize": 424,
+          "bodySize": 0
+        },
+        "response": {
+          "status": 404,
+          "statusText": "Not Found",
+          "httpVersion": "HTTP/1.1",
+          "headers": [
+            {
+              "name": "Date",
+              "value": "Fri, 18 Sep 2015 07:44:20 GMT"
+            },
+            {
+              "name": "Server",
+              "value": "ngx_openresty/1.0.4"
+            },
+            {
+              "name": "Connection",
+              "value": "close"
+            },
+            {
+              "name": "Content-Length",
+              "value": "578"
+            },
+            {
+              "name": "Content-Type",
+              "value": "text/html"
+            }
+          ],
+          "cookies": [],
+          "content": {
+            "size": 578,
+            "mimeType": "text/html",
+            "compression": 0,
+            "text": "<html>\r\n<head><title>404 Not Found</title></head>\r\n<body bgcolor=\"white\">\r\n<center><h1>404 Not Found</h1></center>\r\n<hr><center>ngx_openresty/1.0.4</center>\r\n</body>\r\n</html>\r\n<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n<!-- a padding to disable MSIE and Chrome friendly error page -->\r\n"
+          },
+          "redirectURL": "",
+          "headersSize": 157,
+          "bodySize": 578,
+          "_transferSize": 735
+        },
+        "cache": {},
+        "timings": {
+          "blocked": 0.624999986030161,
+          "dns": 34.199999994598336,
+          "connect": 33.878000016557,
+          "send": 0.05999999120840016,
+          "wait": 3149.574999988546,
+          "receive": 0.9360000258297987,
+          "ssl": -1
+        },
+        "connection": "483912",
+        "pageref": "page_1"
+      }
+    ]
+  }
+}
